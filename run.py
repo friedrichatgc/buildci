e8bd6a9ab792bbc1bd68cdeabf596720cf549654
Markus Friedrich <fm12111+github@googlemail.com>
2024-12-11 21:48:47 +0100

    test

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 import subprocess
 
-subprocess.check_call(["g++", "-o", "main.exe", "main.cc"])
-subprocess.check_call(["./main.exe"])
-subprocess.check_call(["rm", "./main.exe"])
+try:
+  subprocess.check_call(["g++.exe", "-o", "main.exe", "main.cc"])
+  subprocess.check_call(["./main.exe"])
+  subprocess.check_call(["/usr/bin/rm.exe", "./main.exe"])
+except:
+  pass
+
+try:
+  subprocess.check_call(["/ucrt64/bin/g++.exe", "-o", "main.exe", "main.cc"])
+  subprocess.check_call(["./main.exe"])
+  subprocess.check_call(["/usr/bin/rm.exe", "./main.exe"])
+except:
+  pass
 
 print("DONE")

6e5695685e289d0b0dabeb8ae18f7fbc93bdf1ca
Markus Friedrich <fm12111+github@googlemail.com>
2024-12-15 19:58:28 +0100

    tet

--- Dockerfile
+++ Dockerfile
@@ -19,13 +19,61 @@ SHELL ["c:\\msys64\\usr\\bin\\bash.exe", "-l", "-c"]
 # run msys2 the first time -> initial setup
 RUN "echo DONE"
 
+#mfmf
+RUN "which wget"
+run "which rsync"
+#mfmf
+
 # install/update msys2
-ARG MSYS2INSTALLERDOWNLOAD=0
-ARG MSYS2INSTALLERUPLOAD=1
-ARG MSYS2INSTALLERDB=https://sdlkfj.x
-ARG MSYS2INSTALLERCACHE=https://sdlkfj.x
+ARG MSYS2INSTALLERDB=dockeruser@www.mbsim-env.de:1122:/data/databasemedia/msys2db/
+ARG MSYS2INSTALLERCACHE=dockeruser@www.mbsim-env.de:1122:/data/databasemedia/msys2cache/
 COPY install.sh c:/msys64/context/install.sh
-RUN /context/install.sh
+RUN /context/install.sh \
+  dos2unix \
+  patch \
+  unzip \
+  mingw-w64-ucrt-x86_64-7zip \
+  mingw-w64-ucrt-x86_64-swig \
+  mingw-w64-ucrt-x86_64-arpack \
+  mingw-w64-ucrt-x86_64-autotools \
+  mingw-w64-ucrt-x86_64-boost \
+  mingw-w64-ucrt-x86_64-cmake \
+  mingw-w64-ucrt-x86_64-doxygen \
+  mingw-w64-ucrt-x86_64-gcc \
+  mingw-w64-ucrt-x86_64-graphviz \
+  mingw-w64-ucrt-x86_64-hdf5 \
+  mingw-w64-ucrt-x86_64-octave \
+  mingw-w64-ucrt-x86_64-openblas \
+  mingw-w64-ucrt-x86_64-python-numpy \
+  mingw-w64-ucrt-x86_64-python-sympy \
+  mingw-w64-ucrt-x86_64-python-scipy \
+  mingw-w64-ucrt-x86_64-python-matplotlib \
+  mingw-w64-ucrt-x86_64-python-h5py \
+  mingw-w64-ucrt-x86_64-python-psutil \
+  mingw-w64-ucrt-x86_64-python-pygithub \
+  mingw-w64-ucrt-x86_64-python-paramiko \
+  mingw-w64-ucrt-x86_64-pyside2 \
+  mingw-w64-ucrt-x86_64-pyside2-tools \
+  mingw-w64-ucrt-x86_64-qwt-qt5 \
+  mingw-w64-ucrt-x86_64-diffutils \
+  mingw-w64-ucrt-x86_64-soqt-qt5 \
+  mingw-w64-ucrt-x86_64-spooles \
+  mingw-w64-ucrt-x86_64-xalan-c \
+  mingw-w64-ucrt-x86_64-xerces-c \
+  mingw-w64-ucrt-x86_64-ccache \
+  mingw-w64-ucrt-x86_64-python-pip \
+  mingw-w64-ucrt-x86_64-python-asgiref \
+  mingw-w64-ucrt-x86_64-python-pytz \
+  mingw-w64-ucrt-x86_64-python-sqlparse \
+  mingw-w64-ucrt-x86_64-python-requests \
+  mingw-w64-ucrt-x86_64-python-pyjwt \
+  mingw-w64-ucrt-x86_64-python-cryptography \
+  mingw-w64-ucrt-x86_64-python-cffi \
+  mingw-w64-ucrt-x86_64-python-defusedxml \
+  mingw-w64-ucrt-x86_64-python-oauthlib \
+  mingw-w64-ucrt-x86_64-python-psycopg2 \
+  mingw-w64-ucrt-x86_64-gcc-fortran \
+  mingw-w64-ucrt-x86_64-python-requests-oauthlib
 
 # Install pip packages
 RUN "python.exe -m pip install --upgrade pip==24.0 && python.exe -m pip install django==3.2 django-allauth==0.55 django-octicons==1.0"
--- install.sh
+++ install.sh
@@ -1,90 +1,24 @@
 #!/bin/bash
 
-#mfmf
-which wget
-which tar
-which gzip
-which bzip2
-which gunzip
-which bunzip2
-#mfmf
+RSYNC_PASSWORD=$mbsimenvsec_filestoragePassword
 
-if [ $MSYS2INSTALLERDOWNLOAD -eq 1 ]; then
-  # download/install msys2 package db
-  wget $MSYS2INSTALLERDB -O msys2db.tar.bz2
-  tar -C / -xjf msys2db.tar.bz2
-  rm msys2db.tar.bz2
+# download/install msys2 package db
+rsync -r $MSYS2INSTALLERDB/ /var/lib/pacman/sync/
 
-  # download/install msys2 package cache
-  wget $MSYS2INSTALLERCACHE -O msys2cache.tar.bz2
-  tar -C / -xjf msys2cache.tar.bz2
-  rm msys2cache.tar.bz2
-fi
+# download/install msys2 package cache
+rsync -r $MSYS2INSTALLERCACHE/ /var/cache/pacman/pkg/
 
 # update install msys2 packages according package db
 pacman --noconfirm -Suu
 
 # update install new mys2 packages according package db
-pacman --noconfirm -S \
-  dos2unix \
-  patch \
-  unzip \
-  mingw-w64-ucrt-x86_64-7zip \
-  mingw-w64-ucrt-x86_64-swig \
-  mingw-w64-ucrt-x86_64-arpack \
-  mingw-w64-ucrt-x86_64-autotools \
-  mingw-w64-ucrt-x86_64-boost \
-  mingw-w64-ucrt-x86_64-cmake \
-  mingw-w64-ucrt-x86_64-doxygen \
-  mingw-w64-ucrt-x86_64-gcc \
-  mingw-w64-ucrt-x86_64-graphviz \
-  mingw-w64-ucrt-x86_64-hdf5 \
-  mingw-w64-ucrt-x86_64-octave \
-  mingw-w64-ucrt-x86_64-openblas \
-  mingw-w64-ucrt-x86_64-python-numpy \
-  mingw-w64-ucrt-x86_64-python-sympy \
-  mingw-w64-ucrt-x86_64-python-scipy \
-  mingw-w64-ucrt-x86_64-python-matplotlib \
-  mingw-w64-ucrt-x86_64-python-h5py \
-  mingw-w64-ucrt-x86_64-python-psutil \
-  mingw-w64-ucrt-x86_64-python-pygithub \
-  mingw-w64-ucrt-x86_64-python-paramiko \
-  mingw-w64-ucrt-x86_64-pyside2 \
-  mingw-w64-ucrt-x86_64-pyside2-tools \
-  mingw-w64-ucrt-x86_64-qwt-qt5 \
-  mingw-w64-ucrt-x86_64-diffutils \
-  mingw-w64-ucrt-x86_64-soqt-qt5 \
-  mingw-w64-ucrt-x86_64-spooles \
-  mingw-w64-ucrt-x86_64-xalan-c \
-  mingw-w64-ucrt-x86_64-xerces-c \
-  mingw-w64-ucrt-x86_64-ccache \
-  mingw-w64-ucrt-x86_64-python-pip \
-  mingw-w64-ucrt-x86_64-python-asgiref \
-  mingw-w64-ucrt-x86_64-python-pytz \
-  mingw-w64-ucrt-x86_64-python-sqlparse \
-  mingw-w64-ucrt-x86_64-python-requests \
-  mingw-w64-ucrt-x86_64-python-pyjwt \
-  mingw-w64-ucrt-x86_64-python-cryptography \
-  mingw-w64-ucrt-x86_64-python-cffi \
-  mingw-w64-ucrt-x86_64-python-defusedxml \
-  mingw-w64-ucrt-x86_64-python-oauthlib \
-  mingw-w64-ucrt-x86_64-python-psycopg2 \
-  mingw-w64-ucrt-x86_64-gcc-fortran \
-  mingw-w64-ucrt-x86_64-python-requests-oauthlib
+pacman --noconfirm -S "$@"
 
-if [ $MSYS2INSTALLERUPLOAD -eq 1 ]; then
-  # pack/upload msys2 package cache
-  tar -C / -cjf msys2cache.tar.bz2 /var/cache/pacman/pkg/
-  ls -l msys2cache.tar.bz2 #mfmf
-  #mfmf wput $MSYS2INSTALLERCACHE -O msys2cache.tar.bz2
-  rm msys2cache.tar.bz2
-  
-  # pack/upload msys2 package db
-  tar -C / -cjf msys2db.tar.bz2 /var/lib/pacman/sync/
-  ls -l msys2db.tar.bz2 #mfmf
-  #mfmf wput $MSYS2INSTALLERDB -O msys2db.tar.bz2
-  rm msys2db.tar.bz2
-fi
+# pack/upload msys2 package cache
+rsync -r --delete /var/cache/pacman/pkg/ $MSYS2INSTALLERCACHE/
+
+# pack/upload msys2 package db
+rsync -r --delete /var/lib/pacman/sync/ $MSYS2INSTALLERDB/
 
 # remove msys2 cache
 pacman --noconfirm -Scc

6c461a86fd4fa307035d6c0561a056b7655e08c2
Markus Friedrich <fm12111+github@googlemail.com>
2024-12-17 23:10:18 +0100

    test

---
+++ msys2_install1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo mfmf1
+exit 1 #mfmf
+
+set -e
+set -o pipefail
+
+if true; then
+  # download/install msys2 package db
+  wget --no-verbose $MSYS2INSTALLERURI/db.tar.gz
+  tar -C /var/lib/pacman/sync/ -xzf $PWD/db.tar.gz
+  rm -f db.tar.gz
+
+  # download/install msys2 package cache
+  wget --no-verbose $MSYS2INSTALLERURI/cache.tar.gz
+  tar -C /var/cache/pacman/pkg/ -xzf $PWD/cache.tar.gz
+  rm -f cache.tar.gz
+fi
+
+# update install msys2 packages according package db
+if true; then
+  pacman --noconfirm -Suu
+else
+  pacman --noconfirm -Syuu
+fi
--- msys2_install2.sh
+++ msys2_install2.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
+echo mfmf2 "$@"
+exit 0 #mfmf
 
 set -e
 set -o pipefail
-
-if true; then
-  # download/install msys2 package db
-  wget $MSYS2INSTALLERURI/db.tar.gz
-  tar -C /var/lib/pacman/sync/ -xzf $PWD/db.tar.gz
-  rm -f db.tar.gz
-
-  # download/install msys2 package cache
-  wget $MSYS2INSTALLERURI/cache.tar.gz
-  tar -C /var/cache/pacman/pkg/ -xzf $PWD/cache.tar.gz
-  rm -f cache.tar.gz
-fi
 
 # update install msys2 packages according package db
 if true; then
